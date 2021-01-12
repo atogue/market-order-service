cc92f0478522bb57d047a6aa6911a45e15a4e029
Anicet Togue <atogue@gmail.com>
2021-01-12 15:23:59 +0100

    create order model

---
+++ src/main/java/org/craftchain/market/order/entity/Order.java
@@ -0,0 +1,23 @@
+package org.craftchain.market.order.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORDER")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
