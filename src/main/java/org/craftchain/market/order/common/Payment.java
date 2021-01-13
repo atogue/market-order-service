9aa66a1158339cf7dd90dd4af151755863118030
Anicet Togue <atogue@gmail.com>
2021-01-13 12:03:12 +0100

    create payment model to bind on payment service specifications

---
+++ src/main/java/org/craftchain/market/order/common/Payment.java
@@ -0,0 +1,20 @@
+package org.craftchain.market.order.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    private int paymentId;
+    private String status;
+    private String transactionId;
+    private int orderId;
+    private double amount;
+    private Date date;
+}
